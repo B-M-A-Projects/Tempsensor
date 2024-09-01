556ceaa308d449f020197f88fdf7f28c51444c7e
BMA <martin.andersson.lkpg@gmail.com>
2024-09-01 21:16:42 +0200

    Rewritten project

--- tempsensor.py
+++ tempsensor.py
@@ -4,10 +4,10 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 
 # store data in influx database
 def StoreInDatabase(p):
-    influxdb_bucket = "tempsensor"
-    influxdb_org = "BMA"
-    influxdb_token = "ncE41R29bvQLeUNHjNXs6-GOM6lctlP93ymZXJMl5IqUHxkcts45E228jwESCm2FC_nQQWBbJxywGYOdOJj5rw=="
-    influxdb_url = "http://192.168.50.239:8086"
+    influxdb_bucket = "xxxxxxxxxxxxx"
+    influxdb_org = "xxxxxx"
+    influxdb_token = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    influxdb_url = "xxxxxxxxxxxxxxxxxxxxxxxxx"
 
     client = influxdb_client.InfluxDBClient(url=influxdb_url, token=influxdb_token, org=influxdb_org)
 
